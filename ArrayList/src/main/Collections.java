c2c1f8734fdfa2b196254689b52d5658a7b5a4ac
Vaqif Dadaszade <vaqifdadaszade012@gmail.com>
2022-07-12 17:18:15 +0400

    Compalte task

--- ArrayList/src/main/Collections.java
+++ ArrayList/src/main/Collections.java
@@ -5,15 +5,15 @@ import java.util.List;
 
 public class Collections {
 	public static void main(String[] args) {
-	
-	List<Integer> list=new ArrayList<Integer> ();
-	list.add(1);
-	list.add(3);
-	list.add(6);
-	list.add(9);
-	for(int i =0;i<list.size();i++) {
-			System.out.println(list.get(i) * 2);
-	}                 
+
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(3);
+		list.add(6);
+		list.add(9);
 		System.out.println(list);
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i) * 2);
+		}
 	}
 }
