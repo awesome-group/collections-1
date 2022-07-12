12019012e5c98249edeb4a5fc9b909ca9df98de2
Vaqif Dadaszade <vaqifdadaszade012@gmail.com>
2022-07-12 17:10:53 +0400

    Complate task

--- ArrayList/src/main/Collections.java
+++ ArrayList/src/main/Collections.java
@@ -12,9 +12,7 @@ public class Collections {
 	list.add(6);
 	list.add(9);
 	for(int i =0;i<list.size();i++) {
-		if(list.get(i)>0) {
 			System.out.println(list.get(i) * 2);
-		}
 	}                 
 		System.out.println(list);
 	}
